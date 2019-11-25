12153b4e60af39f42f6f9b70c8d0af9d965c9841
Joao Sousa <joaosousa.pt@gmail.com>
2019-11-25 13:21:54 +0000

    Added more C++ exercise solutions

---
+++ C++/OtherConcepts/c++ClassTemplates.cpp
@@ -0,0 +1,61 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+#define concatenate add
+template <class T>
+class AddElements{ 
+public:
+    T elem;
+    AddElements(T param) {
+        elem = param;
+    }
+    T add(T elem2) {
+        return elem + elem2;
+    }
+};
+
+// Exercise is most likely broken. Found a "work around" in the "Discussions" tab:
+int start_up() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+
+int static r = start_up();
+
+#define endl '\n';
+// ("fix" ends here)
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
---
+++ C++/OtherConcepts/operatorOverloading.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Matrix {
+public:
+    vector< vector<int> > a;
+    Matrix operator+(Matrix & m){
+        for (size_t row = 0; row < a.size(); ++row) {
+            for (size_t col = 0; col < a.at(0).size(); ++col) {
+                this->a.at(row).at(col) += m.a.at(row).at(col);
+            }
+        }
+        return *this;
+    }
+};
+
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
